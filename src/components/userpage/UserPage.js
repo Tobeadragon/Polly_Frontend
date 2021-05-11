86436abdbd9da565522bad13da32c777012795c8
ElhamHashemi <elhamhashemi56@yahoo.com>
2021-05-11 22:44:27 +0200

    etwas css

--- src/components/userpage/UserPage.js
+++ src/components/userpage/UserPage.js
@@ -3,6 +3,8 @@ import { withRouter, Link } from "react-router-dom";
 import "./UserPage.css";
 import { Button } from "reactstrap";
 import axios from "axios";
+import {RiSurveyFill,RiDeleteBin6Line} from 'react-icons/ri'
+
 
 const UserPage = (props) => {
   const [umfragen, setUmfragen] = useState([]);
@@ -26,7 +28,7 @@ const UserPage = (props) => {
       .catch((error) => {
         console.log(error);
       });
-  }, []);
+  }, [umfragen]);
 
   const GoCreate = () => {
     props.history.push("./umfrage");
@@ -41,41 +43,80 @@ const UserPage = (props) => {
     localStorage.setItem("umfrage_id", id);
   };
 
+
+  const deleteUmfrage = (id) => {
+    console.log("click", id)
+    let token = localStorage.getItem("user_token");
+    console.log(token);
+    const headers = {
+      "Content-Type": "application/json",
+      authorization: `Bearer ${token}`,
+    };
+    axios
+    .delete(`${process.env.REACT_APP_BACKENDURL}/umfrage/${id}`, { headers: headers })
+    .then((response) => {
+      console.log(response);
+      console.log(response.data);
+      // setUmfragen(umfragen.filter((item) => item.id !== umfragen.id))
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  }
+
   return (
     <div>
       <main className="UserPage">
         <header className="UserHeader">Wilkommen {name}</header>
         <div className="SubHeader">
           {umfragen.length > 0 ? (
-            <p>Klicken Sie auf die Umfrage, um sich die Ergebnisse anzeigen zu lassen.</p>
+            <p className='umfrageTitr'>Klicken Sie auf die Umfrage, um sich die Ergebnisse anzeigen zu lassen.</p>
           ) : (
-            <p>Erstellen Sie ihre Umfrage</p>
-          )}
+              <p>Erstellen Sie ihre Umfrage</p>
+            )}
         </div>
-
-        <div >
+        <div className='centerContain'>
+        <div className='umfrageContainer'>
           <div className="UmfrageListTitel">Ihre Umfragen</div>
+          <div className='listTeil'>
           <ul className="UmfrageList">
             {umfragen.map((umf, index) => (
-              <Link to="/auswertung" kex={index} className="UserUmfrageList">
-                <li onClick={() => saveUmfrageId(umf._id)} key={index}>
-                  <h5>{umf.titel}</h5>
-                </li>
-              </Link>
+
+              <div className='divContainer'>
+                <div className='loopContainer'>
+                  <Link to="/auswertung" key={index} className="UserUmfrageList">
+                    <div><RiSurveyFill className='iconServey2' /></div>
+                    <div className='UserUmfrageListLi '><li onClick={() => saveUmfrageId(umf._id)} key={index} className='itemUmfrage' >
+                      {umf.titel}
+                    </li></div>
+                  </Link>
+                </div>
+                <div className='div3'>
+                  <button className='trash' onClick={()=>deleteUmfrage(umf._id)}><RiDeleteBin6Line className='delIcon' /></button>
+                 
+                </div>
+              </div>
+              
             ))}
+             
           </ul>
+          </div>
+        </div>
         </div>
         <div class="d-flex justify-content-center">
-        <Button
-          color="primary"
-          onClick={() => GoCreate()}
-          className="UserButton"
-          
-        >
-          Neue Umfrage erstellen
+          <Button
+            color="primary"
+            onClick={() => GoCreate()}
+          // className="UserButton"
+
+          >
+            Neue Umfrage erstellen
         </Button>
         </div>
+        <p className='hilfeTxt'>Um Ihre Umfrage zu löschen klicken Sie <RiDeleteBin6Line/>.</p>
+      
       </main>
+
     </div>
   );
 };
